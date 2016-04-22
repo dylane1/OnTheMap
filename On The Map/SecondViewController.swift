ee59139f7104e384950b21ce3687fd805284ca8a
Dylan <dylan@dylanedwards.net>
2016-04-22 12:21:31 -0700

    Initial Commit

---
+++ On The Map/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  On The Map
+//
+//  Created by Dylan Edwards on 4/22/16.
+//  Copyright © 2016 Slinging Pixels Media. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
