b88cda62991916ffce1ecbc7b1e9332432401082
Dylan <dylan@dylanedwards.net>
2016-05-20 10:42:39 -0700

    feat: Adds Login View & Login View Controller

---
+++ On The Map/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  On The Map
+//
+//  Created by Dylan Edwards on 5/20/16.
+//  Copyright © 2016 Slinging Pixels Media. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ On The Map/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Dylan Edwards on 5/20/16.
+//  Copyright © 2016 Slinging Pixels Media. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    private var loginView: LoginView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loginView = view as! LoginView
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
