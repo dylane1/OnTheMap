6f36e2705ad2f54f6f86d21ec9d18d516ba4c8ee
Dylan <dylan@dylanedwards.net>
2018-06-08 09:24:42 -0700

    refactor: updates to Swift 3 syntax

--- On The Map/Reachability.swift
+++ On The Map/Reachability.swift
@@ -13,28 +13,26 @@ import SystemConfiguration
 open class Reachability {
     
     class func isConnectedToNetwork() -> Bool {
+        
         var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
         zeroAddress.sin_family = sa_family_t(AF_INET)
         
         guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
-            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
         }) else {
             return false
         }
         
-        var flags : SCNetworkReachabilityFlags = []
+        var flags: SCNetworkReachabilityFlags = []
         if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
             return false
         }
         
         let isReachable = flags.contains(.reachable)
         let needsConnection = flags.contains(.connectionRequired)
-        
-        // For Swift 3, replace the last two lines by
-        // let isReachable = flags.contains(.reachable)
-        // let needsConnection = flags.contains(.connectionRequired)
-        
         
         return (isReachable && !needsConnection)
     }
